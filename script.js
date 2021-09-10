23b983f513cb15030ac96ce964d4b62a1a727548
thomkorteweg <74902263+ThomKorteweg@users.noreply.github.com>
2021-09-10 08:55:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+class Ball {
+  constructor(x,y,w,h,vx,vy)
+   this.h = h;
+   this.w = w;
+   this.x = x;
+   this.y = y;
+   this.vx = vx
+   this.vy = vy
+drawball() {
+  createCanvas(400,400);
+  backgroundcolour(0,250,32)
+}
+ball111(250,50,30,30.4,6)
+}
+
+drawball()
